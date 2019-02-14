1f7aa11d7207145d8119ab34f71cd302f58f78b1
wzqwangyoucao <1737481406@qq.com>
2019-02-14 11:46:18 +0800

    完成小组件，未优化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SwitchEle v-model="value1" activeColor="#13ce66" inactiveColor="#ff4949" @change="readVal"/>
+    <SwitchEle v-model="value2" activeColor="#007766" inactiveColor="#000049" @change="readVal"/>
+  </div>
+</template>
+
+<script>
+import SwitchEle from './components/Switch';
+export default {
+  name: 'App',
+  components: {SwitchEle},
+  data () {
+    return {
+      value1: true,
+      value2: false
+    };
+  },
+  methods: {
+    readVal () {
+      console.log(this.value1);
+    }
+  }
+};
+</script>
+<style>
+</style>
