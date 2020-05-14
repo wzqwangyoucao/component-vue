e609c5d13eb94ecb7fe9eab4de4f04f7a5dd2b50
wangzq11 <1737481406@qq.com>
2020-05-14 17:19:33 +0800

    路由优化

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-05-13 19:49:13
- * @LastEditTime: 2020-05-13 22:29:13
+ * @LastEditTime: 2020-05-14 17:19:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /component-vue/src/router/router.js
  */
 export default [{
+  path: '/',
+  redirect: '/table'
+}, {
   path: '/table',
   component: () => import('@/container/table.vue')
 }];
